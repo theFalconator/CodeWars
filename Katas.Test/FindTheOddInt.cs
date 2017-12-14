d69bd71bf1228e5ca9e7f4289abc6a638527742d
theFalconator <falcon.nick@gmail.com>
2017-12-14 13:36:09 -0600

    Dictionary frequency approach

---
+++ Katas.Test/FindTheOddInt.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using static Katas.FindTheOddInt;
+
+namespace Katas.Test
+{
+    public class FindTheOddInt
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(5, find_it(new[] { 20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5 }));
+        }
+    }
+}
---
+++ Katas/FindTheOddInt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class FindTheOddInt
+    {
+        public static int find_it(int[] seq)
+        {
+            var frequency = new Dictionary<int, int>();
+
+            foreach (var i in seq)
+            {
+                if (frequency.ContainsKey(i))
+                {
+                    frequency[i] += 1;
+                }
+                else
+                {
+                    frequency.Add(i, 1);
+                }
+            }
+
+            return frequency.Keys.First(key => frequency[key] % 2 != 0);
+        }
+    }
+}
