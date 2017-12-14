f25cb45e8829763e80d05be08ca3f66709c40162
theFalconator <falcon.nick@gmail.com>
2017-12-14 11:25:32 -0600

    track individual bills fails test 4 and test 6

--- Katas/VasyaClerkKata.cs
+++ Katas/VasyaClerkKata.cs
@@ -3,28 +3,54 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Katas
 {
-    public class VasyaClerkKata
+    public static class VasyaClerkKata
     {
         public static string Tickets(int[] peopleInLine)
         {
-            
-            var change = new List<int>();
-            
-            
+            var change = new Dictionary<int, int>();
+
+            change.Add(25, 0);
+            change.Add(50, 0);
+            change.Add(100, 0);
+
+
             foreach (var person in peopleInLine)
             {
-                var needed = person - 25;
-                if (needed == 0)
+                var changeNeeded = person - 25;
+                switch (changeNeeded)
                 {
-                    change.Add(person);
-                    continue;
+                    // given 25 dollars
+                    case 0:
+                    {
+                        change[25]++;
+                    }
+                        break;
+                    // given 50 dollars
+                    case 25:
+                    {
+                        if (change[25] > 0)
+                        {
+                            change[25]--;
+                            change[50]++;
+                        }
+                    }
+                        break;
+                    // given 100 dollars
+                    case 100:
+                    {
+                        if (change[25] > 0 && change[50] > 0)
+                        {
+                            change[25]--;
+                            change[50]--;
+                            change[100]++;
+                        }
+                    }
+                        break;
+                    default:
+                        return "NO";
                 }
-                // person needs change, do we have it?
-                if (!change.Contains(needed))
-                    return "NO";
-                change.Add(person);
-
             }
+
             return "YES";
         }
     }
