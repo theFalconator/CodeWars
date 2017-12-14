330171e11016ca7dd4815566161f486615c16e5a
theFalconator <falcon.nick@gmail.com>
2017-12-14 14:35:58 -0600

    refactored solution

--- Katas/Multiples.cs
+++ Katas/Multiples.cs
@@ -10,10 +10,7 @@ namespace Katas
     {
         public static int Solution(int value)
         {
-            if (value == 0)
-                return 0;
-
-            return Enumerable.Range(1, value - 1).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
+            return value == 0 ? 0 : Enumerable.Range(1, value - 1).Where(i => i % 3 == 0 || i % 5 == 0).Sum();
         }
     }
 }
