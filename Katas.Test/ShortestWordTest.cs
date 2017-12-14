6d3a9ea87075aa6e2ec450667bdbc20a0896d9e5
theFalconator <falcon.nick@gmail.com>
2017-12-13 19:43:50 -0600

    LINQ solution

---
+++ Katas.Test/ShortestWordTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Xunit;
+using static Katas.ShortestWordKata;
+
+namespace Katas.Test
+{
+    public class ShortestWordTest
+    {
+        [Fact]
+        public void BasicTests()
+        {
+            Assert.Equal(3, FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.Equal(3, FindShort("turns out random test cases are easier than writing out basic ones"));              
+        }
+
+        [Fact]
+        public void SplitOnSpaces()
+        {
+            var words = new[] {"one", "two", "three"};
+            const string s = "one two three";
+            
+            Assert.Equal(s.Split(' '), words);
+        }
+
+        [Fact]
+        public void LinqFirst()
+        {
+            const string s = "two three";
+            var words = s.Split(' ');
+            
+            Assert.Equal("two", words.OrderBy(word => word.Length).First());
+        }
+    }
+}
---
+++ Katas/ShortestWordKata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas
+{
+    public class ShortestWordKata
+    {
+        public static int FindShort(string words)
+        {
+            return words.Split(' ').OrderByDescending(word => word.Length).Last().Length;
+        }
+    }
+}
