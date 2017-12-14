eb8db91efe2c6aed6f4e0541cff211432fa51661
theFalconator <falcon.nick@gmail.com>
2017-12-13 20:03:44 -0600

    classic for loop approach

---
+++ Katas.Test/CategorizeNewMemberTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using static Katas.CategorizeNewMemberKata;
+
+namespace Katas.Test
+{
+    
+    public class CategorizeNewMemberTest
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(new[] { "Open", "Senior", "Open", "Senior" }, OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+            Assert.Equal(new[] { "Open", "Open", "Open", "Open" }, OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }));
+            Assert.Equal(new[] { "Senior", "Open", "Open", "Open" }, OpenOrSenior(new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } }));    
+        }
+        
+    }
+}
---
+++ Katas/CategorizeNewMemberKata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Katas
+{
+    public static class CategorizeNewMemberKata
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            var result = new List<string>();
+            foreach (var row in data)
+            {
+                var age = row[0];
+                var handicap = row[1];
+
+                if (age >= 55 && handicap > 7)
+                    result.Add("Senior");
+                else
+                {
+                    result.Add("Open");
+                }
+            }
+
+            return result;
+        }
+    }
+}
