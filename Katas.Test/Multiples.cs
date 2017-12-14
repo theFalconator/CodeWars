f5dd76fdd308bef0a61df677e5c9fec6dfc020d1
theFalconator <falcon.nick@gmail.com>
2017-12-14 14:18:43 -0600

    linq solution

---
+++ Katas.Test/Multiples.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using static Katas.Multiples;
+
+namespace Katas.Test
+{
+    public class Multiples
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(23, Solution(10));
+        }
+    }
+}
---
+++ Katas/Multiples.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class Multiples
+    {
+        private static readonly IEnumerable<int> multiples = new[] {3, 5};
+
+        public static int Solution(int value)
+        {
+            return Enumerable.Range(1, value-1).Sum(i => multiples.Where(m => i % m == 0).Sum(m => i));
+        }
+    }
+}
