78f74619e5d9ce153ef8ab19d7cf79589d00d404
theFalconator <falcon.nick@gmail.com>
2017-12-14 11:41:12 -0600

    linq solution

---
+++ Katas.Test/ExesAndOhs.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Xunit;
+using static Katas.ExesAndOhs;
+
+namespace Katas.Test
+{
+    public class ExesAndOhs
+    {
+        [Fact]
+        public void ExampleTests()
+        {
+            
+            Assert.Equal(true, XO("xo"));
+            Assert.Equal(true, XO("xxOo"));
+            Assert.Equal(false, XO("xxxm"));
+            Assert.Equal(false, XO("Oo"));
+            Assert.Equal(false, XO("ooom"));
+        }
+    }
+}
---
+++ Katas/ExesAndOhs.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Katas
+{
+    public static class ExesAndOhs
+    {
+        public static bool XO(string input)
+        {
+            var xs = input.Count(c => c == 'o' || c == 'O');
+            var os = input.Count(c => c == 'x' || c == 'X');
+            return xs == os;
+        }
+    }
+}
