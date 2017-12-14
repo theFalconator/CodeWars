9910594c383a4f339e97b6769d6194d16188599c
theFalconator <falcon.nick@gmail.com>
2017-12-14 14:02:14 -0600

    initial solution

--- Katas/PersistentBugger.cs
+++ Katas/PersistentBugger.cs
@@ -1,10 +1,31 @@
-﻿namespace Katas
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
 {
     public class PersistentBugger
     {
+        public static IEnumerable<int> SplitIntoDigits(long n)
+        {
+            var s = n.ToString();
+            return s.Select(digit => int.Parse(digit.ToString()));
+        }
         public static int Persistence(long n)
         {
-            return 1;
+            if (n < 10)
+                return 0;
+            
+            
+            var result = SplitIntoDigits(n);
+            var product = result.Aggregate(1, (a, b) => a * b);
+            var count = 1;
+            while (product > 9)
+            {
+                result = SplitIntoDigits(product);
+                count++;
+                product = result.Aggregate(1, (a, b) => a * b);
+            }
+            return count;
         }
     }
 }
