59f85321c399d942e46b395ffbd6ec15b63a2e4f
theFalconator <falcon.nick@gmail.com>
2017-12-14 11:12:29 -0600

    track change attempt fails test 6

--- Katas/VasyaClerkKata.cs
+++ Katas/VasyaClerkKata.cs
@@ -1,10 +1,31 @@
-﻿namespace Katas
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Katas
 {
     public class VasyaClerkKata
     {
         public static string Tickets(int[] peopleInLine)
         {
-            return "NO";
+            
+            var change = new List<int>();
+            
+            
+            foreach (var person in peopleInLine)
+            {
+                var needed = person - 25;
+                if (needed == 0)
+                {
+                    change.Add(person);
+                    continue;
+                }
+                // person needs change, do we have it?
+                if (!change.Contains(needed))
+                    return "NO";
+                change.Add(person);
+
+            }
+            return "YES";
         }
     }
 }
