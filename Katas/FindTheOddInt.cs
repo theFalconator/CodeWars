d0884427d9e14c9efabc9e1ec2830e1b4c287921
theFalconator <falcon.nick@gmail.com>
2017-12-14 13:38:42 -0600

    refactored using LINQ groupby

--- Katas/FindTheOddInt.cs
+++ Katas/FindTheOddInt.cs
@@ -7,21 +7,7 @@ namespace Katas
     {
         public static int find_it(int[] seq)
         {
-            var frequency = new Dictionary<int, int>();
-
-            foreach (var i in seq)
-            {
-                if (frequency.ContainsKey(i))
-                {
-                    frequency[i] += 1;
-                }
-                else
-                {
-                    frequency.Add(i, 1);
-                }
-            }
-
-            return frequency.Keys.First(key => frequency[key] % 2 != 0);
+            return seq.GroupBy(i => i).Single(group => group.Count() % 2 != 0).Key;
         }
     }
 }
