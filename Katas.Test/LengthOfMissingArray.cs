3c5ee845767a2deff3aa536f715f6e57e0283c36
theFalconator <falcon.nick@gmail.com>
2018-01-12 17:32:23 -0600

    codewars throws nullpointerexception

---
+++ Katas.Test/LengthOfMissingArray.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using static Katas.LengthOfMissingArray;
+
+namespace Katas.Test
+{
+    public class LengthOfMissingArray
+    {
+        [Fact]
+        public void BasicTests()
+        {
+            Assert.Equal(3, GetLengthOfMissingArray(new object[][] { new object[] { 1, 2 }, new object[] { 4, 5, 1, 1 }, new object[] { 1 }, new object[] { 5, 6, 7, 8, 9 }} ));
+            Assert.Equal(2, GetLengthOfMissingArray(new object[][] { new object[] { 5, 2, 9 }, new object[] { 4, 5, 1, 1 }, new object[] { 1 }, new object[] { 5, 6, 7, 8, 9 }} ));
+            Assert.Equal(2, GetLengthOfMissingArray(new object[][] { new object[] { null }, new object[] { null, null, null } } ));
+            Assert.Equal(5, GetLengthOfMissingArray(new object[][] { new object[] { 'a', 'a', 'a' }, new object[] { 'a', 'a' }, new object[] { 'a', 'a', 'a', 'a' }, new object[] { 'a' }, new object[] { 'a', 'a', 'a', 'a', 'a', 'a' }} ));
+      
+            Assert.Equal(0, GetLengthOfMissingArray(new object[][] { }));            
+        }
+    }
+}
---
+++ Katas/LengthOfMissingArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class LengthOfMissingArray
+    {
+        public static int GetLengthOfMissingArray(object[][] arrayOfArrays)
+        {
+            if (arrayOfArrays == null || arrayOfArrays.Length == 0)
+                return 0;
+            
+            foreach (var array in arrayOfArrays)
+            {
+                if (array.Length == 0 || array.Equals(null))
+                    return 0;
+            }
+            
+            var lengths = arrayOfArrays.Select(array => array.Length).ToList();
+            if (lengths.Count == 0)
+                return 0;
+
+            var allLengths = Enumerable.Range(lengths.Min(), lengths.Max());
+
+            return allLengths.Except(lengths).First();
+        }
+    }
+}
