346f58040372642888f69f416cf690f85c4aab19
theFalconator <falcon.nick@gmail.com>
2018-02-25 08:13:54 -0600

    linq solution

---
+++ Katas.Test/WeightSortTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Katas.Test
+{
+    public class WeightSortTests
+    {
+        [Fact]
+        public void SplitIntoDigits()
+        {
+            const string result = "12345";
+            var nums = result.Select(i => int.Parse(i.ToString()));
+            Assert.Equal(new[] {1,2,3,4,5}, nums);
+        }
+        
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal("2000 103 123 4444 99", WeightForWeight.orderWeight("103 123 4444 99 2000"));
+            Assert.Equal("11 11 2000 10003 22 123 1234000 44444444 9999", WeightForWeight.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+        }
+        
+    }
+}
---
+++ Katas/WeightForWeight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class WeightForWeight
+    {
+        public static string orderWeight(string s)
+        {
+            // split list into individual weights
+            var weights = s.Split(' ');
+            
+            // Populate hashtable with weights and sorted weights
+            var Dict = new Dictionary<string, int>();
+            foreach (var weight in weights)
+            {
+                var sortedWeight = weight.Select(i => int.Parse(i.ToString())).Sum();
+                if(!Dict.ContainsKey(weight))
+                    Dict.Add(weight,sortedWeight);    
+            }
+            
+            // Sort dictionary by value and grab only keys
+            var sorted = weights.OrderBy(i => Dict[i]);
+            // var sorted = Dict.OrderBy(i => i.Value).Select(x=>x.Key);
+            // concatenate list into single string and return
+            return string.Join(" ", sorted);
+        }
+    }
+}
